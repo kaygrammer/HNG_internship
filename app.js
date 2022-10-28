0ce22dd4c2f6852443a8d925a0b85526208011ab
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2022-10-28 14:32:13 +0100

    cor uninstall

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const app = express()
-const cor = require('cors')
+
 
 app.use(express.json())
 
-
-var corsOptions = {
-    origin: '',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
 
 const port = process.env.PORT || 3000
 
 const info = { "slackUsername": "kayode olaoye", "backend": true, "age": 25, "bio": "I am an aspiring software engineer" }
 
-app.get('/', cors(corsOptions), (req, res)=>{
+app.get('/', (req, res)=>{
     res.json(info)
 })
 
