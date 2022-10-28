93e174d92c103ccc77684c1b4f9a5ebb20a4a1c1
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2022-10-28 14:27:59 +0100

    cor update

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express()
+const cor = require('cors')
 
 app.use(express.json())
+
+
+var corsOptions = {
+    origin: 'https://hng-server-task.herokuapp.com/',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 
 const port = process.env.PORT || 3000
 
 const info = { "slackUsername": "kayode olaoye", "backend": true, "age": 25, "bio": "I am an aspiring software engineer" }
 
-app.get('/', (req, res)=>{
+app.get('/', cors(corsOptions), (req, res)=>{
     res.json(info)
 })
 
