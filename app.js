cf050a6ce6bdc8244ea4e0d0784fd57c3d42e9e5
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2022-10-28 12:53:49 +0100

    dd

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express()
+
+app.use(express.json())
+
+const port = 3000
+
+const info = { "slackUsername": "kayode", "backend": true, "age": 25, "bio": "I am an aspiring software engineer" }
+
+app.get('/', (req, res)=>{
+    res.json(info)
+})
+
+const start = async () => {
+    app.listen(port, console.log(`server running on port ${port}`))
+}
+
+start()
