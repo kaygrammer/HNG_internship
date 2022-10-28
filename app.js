6d5d77f7caaf4d909f88728d3f405b8328774259
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2022-10-28 13:27:23 +0100

    heroku update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(express.json())
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const info = { "slackUsername": "kayode olaoye", "backend": true, "age": 25, "bio": "I am an aspiring software engineer" }
 
